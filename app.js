ca76582098a7f1c393eca261faa9f66f0d28cb81
sergioespinoza <sergioarturoespinoza@gmail.com>
2019-03-13 10:52:15 -0600

    meeting commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch");
 var express = require('express');
 var bodyParser = require('body-parser');
+var luz=false;
 
 var app = express();
 var port = process.env.PORT || 1337;
@@ -15,7 +16,50 @@ app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
 
+app.post('/meeting', function (req, res, next) {
+if (!luz) {
+  fetch("https://maker.ifttt.com/trigger/encenderFoco/with/key/d2gEXI2jzz6CNGYc66_W8i", {mode: 'no-cors'})
+.then(function(response) {
+  console.log('Request successful',response);
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', light on.'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+}).catch(function(error) {
+  console.log('Request failed', error)
+});
+luz=true;
+} else {
+  fetch("https://maker.ifttt.com/trigger/apagarFoco/with/key/d2gEXI2jzz6CNGYc66_W8i", {mode: 'no-cors'})
+.then(function(response) {
+  console.log('Request successful',response);
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', light off.'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+}).catch(function(error) {
+  console.log('Request failed', error)
+});
+}
 
+
+
+});
+
+
+/*
 app.post('/turn-on-light', function (req, res, next) {
   fetch("https://maker.ifttt.com/trigger/encenderFoco/with/key/d2gEXI2jzz6CNGYc66_W8i", {mode: 'no-cors'})
 .then(function(response) {
@@ -54,4 +98,4 @@ app.post('/turn-off-light', function (req, res, next) {
   console.log('Request failed', error)
 });
 
-});
+}); */
